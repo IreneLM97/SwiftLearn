928f609936ab05a3359ac4a040b5a4ea48a31ec1
IreneLM97 <irenelm1997@gmail.com>
2024-01-30 11:51:04 +0100

    Arreglado scroll en pantalla

--- app/src/main/java/com/example/swiftlearn/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/swiftlearn/ui/screens/login/LoginScreen.kt
@@ -7,11 +7,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,16 +36,13 @@ object LoginDestination : NavigationDestination {
 fun LoginScreen(
     modifier: Modifier = Modifier
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
     // Diseño de la estructura básica de la pantalla
     Scaffold(
         topBar = {
             // Barra superior personalizada
             SwiftLearnTopAppBar(
                 title = stringResource(id = LoginDestination.titleRes),
-                canNavigateBack = false,
-                scrollBehavior = scrollBehavior
+                canNavigateBack = false
             )
         }
     ) { innerPadding ->
@@ -64,7 +62,8 @@ private fun LoginHeader(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
     ) {
